246213e236ec4f4d328663425ef5ad26eb9a30f6
dragon <975425198@qq.com>
2021-07-02 10:54:45 +0800

    rest json

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -2,15 +2,25 @@ package com.example;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/hello")
 public class ExampleResource {
 
+    /**
+     * 项目是通过quarkus:Dev启动的，无需启动类
+     * 支持热修改，实时生效
+     */
+
+    /**
+     * 默认不支持JSON格式 需要引入resteasy-jackson包(或jsonb)，而后不需要Produces注解默认将变为JSON格式
+     * //@Produces(MediaType.APPLICATION_JSON)
+     */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public Map<String, String> hello() {
+        return new HashMap<>(1) {{
+            put("hello", "World");
+        }};
     }
 }
